18c2a61ecada71c9de32020058bbe8f160e79d91
robertian <1qa2ws3ed>
2015-03-26 11:03:55 -0500

    pooky.py now runs stimuli, makes the csv, flips stimuli based on trial and records frames for analysis

---
+++ pooky.py
@@ -0,0 +1,152 @@
+__author__ = 'ian'
+
+
+# imports
+import sys
+import time
+import csv
+from psychopy import visual, core
+import os
+import cv2
+
+# housekeeping
+clock = core.Clock()
+date = time.strftime('%m%d%Y')
+now = time.strftime('%X')
+
+# open output csv file for writing or appending
+f = date + '_ethotrials.csv'
+
+
+try:
+    fsize = os.stat(f).st_size
+except OSError:
+    w = csv.writer(open(f, 'w'), delimiter=',')
+    w.writerow(['date', 'trial time start', 'trial length', 'stimulus ID', 'trial type', 'stimulus1 screen',
+                'stimulus2 screen'])
+else:
+    if fsize > 0:
+        w = csv.writer(open(f, 'a'), delimiter=',')
+
+
+# presentation windows
+win1 = visual.Window(screen=0, size=(1920, 1080), pos=(0, 0))
+win2 = visual.Window(screen=1, size=(1920, 1080), pos=(0, 0))
+
+
+# list out all stimulus images here
+stim5 = visual.ImageStim(win1, image='5.png', pos=(0, 0.75), colorSpace='rgb', name='5.png')
+stim6 = visual.ImageStim(win2, image='6.png', pos=(0, 0.75), colorSpace='rgb', name='6.png')
+stim7 = visual.ImageStim(win1, image='7.png', pos=(0, 0.75), colorSpace='rgb', name='7.png')
+stim8 = visual.ImageStim(win2, image='8.png', pos=(0, 0.75), colorSpace='rgb', name='8.png')
+stim9 = visual.ImageStim(win1, image='9.png', pos=(0, 0.75), colorSpace='rgb', name='9.png')
+stim10 = visual.ImageStim(win2, image='10.png', pos=(0, 0.75), colorSpace='rgb', name='10.png')
+stim12 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
+stim14 = visual.ImageStim(win2, image='14.png', pos=(0, 0.75), colorSpace='rgb', name='14.png')
+stim0 = visual.ImageStim(win1, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
+stim00 = visual.ImageStim(win2, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
+
+
+# stimulus picking tree, adjust trial TypeIDs/stimulus combinations here
+try:
+    stimID = sys.argv[1]
+    if stimID == '5_10':
+        trialstim1 = stim5
+        trialstim2 = stim10
+    if stimID == '6_12':
+        trialstim1 = stim6
+        trialstim2 = stim12
+    if stimID == '7_14':
+        trialstim1 = stim7
+
+        trialstim2 = stim14
+    if stimID == '8_12':
+        trialstim1 = stim8
+        trialstim2 = stim12
+    if stimID == '9_12':
+        trialstim1 = stim9
+        trialstim2 = stim12
+    if stimID == 'acclim':
+        trialstim1 = stim0
+        trialstim2 = stim00
+except IndexError:
+    print "you forgot to say what trial type. defaulting to 'acclim'"
+    trialstim1 = stim0
+    trialstim2 = stim00
+
+
+# training or probe trial/length
+try:
+    ttID = sys.argv[2]
+    if ttID == 'train':
+        tLength = 7 * 60 * 60
+    if ttID == 'probe':
+        tLength = 7 * 60 * 60
+except IndexError:
+    print "you forgot to say if this is a 'train' or 'probe' trial"
+    tLength = 7 * 60
+
+
+# alternate side stimulus is displayed on
+csvf_length = sum(1 for row in w)
+
+if csvf_length % 2 == 0:
+    trialstim1.win = win2
+    trialstim2.win = win1
+else:
+    trialstim1.win = win1
+    trialstim2.win = win2
+
+
+#blit displays
+def display():
+    trialstim1.draw()
+    trialstim2.draw()
+    win1.flip()
+    win2.flip()
+
+
+# set up captures
+cap = cv2.VideoCapture(0)
+width = cap.get(3)
+height = cap.get(4)
+
+cap2 = cv2.VideoCapture(1)
+width2 = cap2.get(3)
+height2 = cap2.get(4)
+
+shotdir1 = os.getcwd()+'/CAM1'
+shotdir2 = os.getcwd()+'/CAM2'
+shot_idx = 0
+
+# do it
+while cap.isOpened():
+    for FrameN in range(tLength):
+
+        ret2, frame2 = cap2.read()
+        ret, frame = cap.read()
+
+
+        fn = '%s%03d.bmp' % (shotdir1, shot_idx)
+        fn1 = '%s%03d.bmp' % (shotdir2, shot_idx)
+        cv2.imwrite(fn, frame)
+        cv2.imwrite(fn1, frame2)
+        if ord('q') == cv2.waitKey(1) & 0xFF:
+            write = False
+            break
+
+        display()
+        shot_idx += 1
+    cap.release()
+
+
+try:
+    w.writerow(
+        [date, now, stimID, ttID, trialstim1.name, trialstim1.win.screen, trialstim2.name, trialstim2.win.screen])
+except NameError:
+    w.writerow([date, now, 'stimID missing', 'ttID missing', trialstim1.name, trialstim1.win.screen, trialstim2.name,
+
+                trialstim2.win.screen])
+
+
+cv2.destroyAllWindows()
