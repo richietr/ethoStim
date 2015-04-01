476a6ff5552b261e0fb0a44d7cae94ffd177f2fb
robertian <1qa2ws3ed>
2015-04-01 16:26:01 -0500

    update for Cap

--- pooky.py
+++ pooky.py
@@ -33,7 +33,6 @@ except OSError:
 else:
     if fsize > 0:
         w = csv.writer(open(f, 'a'), delimiter=',')
-
 #fish specification
 try:
     pesces = sys.argv[3]
@@ -45,42 +44,43 @@ except IndexError:
 win1 = visual.Window(screen=0, size=(1920, 1080), pos=(0, 0))
 win2 = visual.Window(screen=1, size=(1920, 1080), pos=(0, 0))
 
-
-# list out all stimulus images here
-stim5 = visual.ImageStim(win1, image='5.png', pos=(0, 0.75), colorSpace='rgb', name='5.png')
-stim6 = visual.ImageStim(win2, image='6.png', pos=(0, 0.75), colorSpace='rgb', name='6.png')
-stim7 = visual.ImageStim(win1, image='7.png', pos=(0, 0.75), colorSpace='rgb', name='7.png')
-stim8 = visual.ImageStim(win2, image='8.png', pos=(0, 0.75), colorSpace='rgb', name='8.png')
-stim9 = visual.ImageStim(win1, image='9.png', pos=(0, 0.75), colorSpace='rgb', name='9.png')
-stim10 = visual.ImageStim(win2, image='10.png', pos=(0, 0.75), colorSpace='rgb', name='10.png')
-stim12 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-stim14 = visual.ImageStim(win2, image='14.png', pos=(0, 0.75), colorSpace='rgb', name='14.png')
-stim0 = visual.ImageStim(win1, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
-stim00 = visual.ImageStim(win2, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
-
-
 # stimulus picking tree, adjust trial TypeIDs/stimulus combinations here
 try:
     stimID = sys.argv[1]
     if stimID == '5_10':
-        trialstim1 = stim5
-        trialstim2 = stim10
+        trialstim1 = visual.ImageStim(win1, image='5.png', pos=(0, 0.75), colorSpace='rgb', name='5.png')
+        trialstim2 = visual.ImageStim(win2, image='10.png', pos=(0, 0.75), colorSpace='rgb', name='10.png')
     if stimID == '6_12':
-        trialstim1 = stim6
-        trialstim2 = stim12
+        trialstim1 = visual.ImageStim(win1, image='6.png', pos=(0, 0.75), colorSpace='rgb', name='6.png')
+        trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
     if stimID == '7_14':
-        trialstim1 = stim7
-
-        trialstim2 = stim14
+        trialstim1 = visual.ImageStim(win1, image='7.png', pos=(0, 0.75), colorSpace='rgb', name='7.png')
+        trialstim2 = visual.ImageStim(win2, image='14.png', pos=(0, 0.75), colorSpace='rgb', name='14.png')
     if stimID == '8_12':
-        trialstim1 = stim8
-        trialstim2 = stim12
+        trialstim1 = visual.ImageStim(win1, image='8.png', pos=(0, 0.75), colorSpace='rgb', name='8.png')
+        trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
     if stimID == '9_12':
-        trialstim1 = stim9
-        trialstim2 = stim12
+        trialstim1 = visual.ImageStim(win1, image='9.png', pos=(0, 0.75), colorSpace='rgb', name='9.png')
+        trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
     if stimID == 'acclim':
-        trialstim1 = stim0
-        trialstim2 = stim00
+        trialstim1 = visual.ImageStim(win1, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
+        trialstim2 = visual.ImageStim(win2, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
+    if stimID == '10_5':
+        trialstim1 = visual.ImageStim(win2, image='5.png', pos=(0, 0.75), colorSpace='rgb', name='5.png')
+        trialstim2 = visual.ImageStim(win1, image='10.png', pos=(0, 0.75), colorSpace='rgb', name='10.png')
+    if stimID == '12_6':
+        trialstim1 = visual.ImageStim(win2, image='6.png', pos=(0, 0.75), colorSpace='rgb', name='6.png')
+        trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
+    if stimID == '14_7':
+        trialstim1 = visual.ImageStim(win2, image='7.png', pos=(0, 0.75), colorSpace='rgb', name='7.png')
+        trialstim2 = visual.ImageStim(win1, image='14.png', pos=(0, 0.75), colorSpace='rgb', name='14.png')
+    if stimID == '12_8':
+        trialstim1 = visual.ImageStim(win2, image='8.png', pos=(0, 0.75), colorSpace='rgb', name='8.png')
+        trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
+    if stimID == '12_9':
+        trialstim1 = visual.ImageStim(win2, image='9.png', pos=(0, 0.75), colorSpace='rgb', name='9.png')
+        trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
+
 except IndexError:
     print "you forgot to say what trial type. defaulting to 'acclim'"
     trialstim1 = stim0
@@ -99,24 +99,12 @@ except IndexError:
     tLength = 7 * 60
 
 
-# alternate side stimulus is displayed on
-csvf_length = sum(1 for row in f)
-
-if csvf_length % 2 == 0:
-    trialstim1.win = win2
-    trialstim2.win = win1
-else:
-    trialstim1.win = win1
-    trialstim2.win = win2
-
-
 #blit displays
 def display():
     trialstim1.draw()
     trialstim2.draw()
     win1.flip()
     win2.flip()
-
 
 # set up captures
 cap = cv2.VideoCapture(0)
@@ -127,27 +115,6 @@ cap2 = cv2.VideoCapture(1)
 width2 = cap2.get(3)
 height2 = cap2.get(4)
 
-
-# do it
-while cap.isOpened():
-    for FrameN in range(tLength):
-
-        ret2, frame2 = cap2.read()
-        ret, frame = cap.read()
-
-
-        fn = '%s/%03d.bmp' % (shotdir1, shot_idx)
-        fn1 = '%s/%03d.bmp' % (shotdir2, shot_idx)
-        cv2.imwrite(fn, frame)
-        cv2.imwrite(fn1, frame2)
-        if ord('q') == cv2.waitKey(1) & 0xFF:
-            break
-
-        display()
-        shot_idx += 1
-    cap.release()
-
-
 try:
     w.writerow([date, now, stimID, ttID, pesces, trialstim1.name, trialstim1.win.screen, trialstim2.name, trialstim2.win.screen])
 except NameError:
@@ -155,5 +122,23 @@ except NameError:
 
                 trialstim2.win.screen])
 
+# do it
+while True:
+    #for FrameN in range(tLength):
+    ch = 0xFF & cv2.waitKey(1)
+    if ch == ord('q'):
+        break
+    ret2, frame2 = cap2.read()
+    ret, frame = cap.read()
 
+    fn = '%s/%07d.bmp' % (shotdir1, shot_idx)
+    fn1 = '%s/%07d.bmp' % (shotdir2, shot_idx)
+    cv2.imwrite(fn, frame)
+    cv2.imwrite(fn1, frame2)
+    display()
+    shot_idx += 1
+
+
+
+cap.release()
 cv2.destroyAllWindows()
