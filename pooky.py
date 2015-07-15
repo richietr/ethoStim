485769cf59e78e83b4a24ddde31e42d73c5df319
robertian <1qa2ws3ed>
2015-07-15 15:05:38 -0500

    Updated for use on MACOS. Including a applescript to reset screen resolution (may not be necessary on all machines)

--- pooky.py
+++ pooky.py
@@ -5,207 +5,180 @@ __author__ = 'ian'
 import sys
 import time
 import csv
-from psychopy import visual, core, event
+from psychopy import visual, core, event, monitors
 import os
 import subprocess
+import random
+import argparse
 
 
-# housekeeping
-clock = core.Clock()
-date = time.strftime('%m%d%Y')
-now = time.strftime('%X')
-nowfolder = time.strftime('%H%M%S')
+class Trial:
 
 
-# open output csv file for writing or appending
+    def __init__(self, date, now, day, session, leftFish, rightFish, highStim, lowStim, cond):
+        self.lftpez = leftFish
+        self.rgtpez = rightFish
+        self.day = day
+        self.session = session
+        self.hstim = highStim
+        self.lstim = lowStim
+        self.conditionside = cond
+        win1 = visual.Window(pos=[0.0,0.0], units='height', winType='pyglet', monitor=None, screen=1, size=[832.0,624.0])
+        win2 = visual.Window(pos=[0.0,0.0], units='height', winType='pyglet', monitor=None, screen=2, size=[832.0,624.0])
+        self.win1 = win1
+        self.win2 = win2
+        self.tLength = 4 * 60
+        self.clock = core.Clock()
+        lfi = leftFish[0]
+        rfi = rightFish[0]
+        coni = cond[0]
 
-f = os.path.join(os.getcwd() + date + '_ethotrials.csv')
-print f
-try:
-    fsize = os.stat(f).st_size
-except OSError:
-    w = csv.writer(open(f, "w+b"))
-    w.writerow(
-        ['date', 'trial time start', 'stimulus ID', 'trial type', 'fish group', 'stimulus1 name', 'stimulus1 screen',
-         'stimulus2 name',
-         'stimulus2 screen'])
-else:
-    if fsize > 0:
-        w = csv.writer(open(f, 'a'), delimiter=',')
-# fish specification
+        print lfi, rfi, coni
+        if lfi == 'H' and rfi == 'L' and coni == 'R':
+            self.stimID = str(highStim)+'_'+str(lowStim)
+        if lfi == 'H' and rfi == 'L' and coni == 'L':
+            self.stimID = str(lowStim)+'_'+str(highStim)
+        if lfi =='L' and rfi == 'H' and coni == 'R':
+            self.stimID = str(lowStim)+'_'+str(highStim)
+        if lfi =='L' and rfi == 'H' and coni == 'L':
+            self.stimID = str(highStim)+'_'+str(lowStim)
+        if lfi =='L' and rfi == 'H' and coni == 'b':
+            self.stimID = str(highStim)+'_'+str(lowStim)
+        if lfi =='H' and rfi == 'L' and coni == 'b':
+            self.stimID = str(highStim)+'_'+str(lowStim)
+        if lfi =='L' and rfi == 'H' and coni == 'n':
+            self.stimID = str(highStim)+'_'+str(lowStim)
+        if lfi =='H' and rfi == 'L' and coni == 'n':
+            self.stimID = str(highStim)+'_'+str(lowStim)
 
-try:
-    pesces = sys.argv[3]
-except IndexError:
-    print "you forgot to say which fish these are (e.g.; 'm1_f3' -[lftf_rghtf]"
-    pesces = 'lf?_rf?'
+        f = os.path.join(os.getcwd() + date + '_ethotrials.csv')
+        try:
+            fsize = os.stat(f).st_size
+        except OSError:
+            self.w = csv.writer(open(f, "w+b"))
+            self.w.writerow(
+                ['date', 'start time', 'day', 'trial session', 'left fish', 'right fish',
+                 'high stim','lowstim','side fed'])
+        else:
+            if fsize > 0:
+                self.w = csv.writer(open(f, 'a'), delimiter=',')
+        try:
 
-try:
-    lftpez, rgtpez = pesces.split("_")
-except NameError:
-    pass
-
-
-# presentation windows
-win1 = visual.Window(screen=0, size=(1024, 768), pos=(0, 0))
-win2 = visual.Window(screen=1, size=(1024, 768), pos=(0, 0))
-
-# stimulus picking tree, adjust trial TypeIDs/stimulus combinations here
-try:
-    stimID = sys.argv[1]
-    if stimID == '5_10':
-        trialstim1 = visual.ImageStim(win1, image='5.png', pos=(0, 0.75), colorSpace='rgb', name='5.png')
-        trialstim2 = visual.ImageStim(win2, image='10.png', pos=(0, 0.75), colorSpace='rgb', name='10.png')
-    if stimID == '6_12':
-        trialstim1 = visual.ImageStim(win1, image='6.png', pos=(0, 0.75), colorSpace='rgb', name='6.png')
-        trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-    if stimID == '7_14':
-        trialstim1 = visual.ImageStim(win1, image='7.png', pos=(0, 0.75), colorSpace='rgb', name='7.png')
-        trialstim2 = visual.ImageStim(win2, image='14.png', pos=(0, 0.75), colorSpace='rgb', name='14.png')
-    if stimID == '8_12':
-        trialstim1 = visual.ImageStim(win1, image='8.png', pos=(0, 0.75), colorSpace='rgb', name='8.png')
-        trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-    if stimID == '9_12':
-        trialstim1 = visual.ImageStim(win1, image='9.png', pos=(0, 0.75), colorSpace='rgb', name='9.png')
-        trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-    if stimID == 'acclim_acclim':
-        trialstim1 = visual.ImageStim(win1, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
-        trialstim2 = visual.ImageStim(win2, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
-    if stimID == '10_5':
-        trialstim1 = visual.ImageStim(win2, image='5.png', pos=(0, 0.75), colorSpace='rgb', name='5.png')
-        trialstim2 = visual.ImageStim(win1, image='10.png', pos=(0, 0.75), colorSpace='rgb', name='10.png')
-    if stimID == '12_6':
-        trialstim1 = visual.ImageStim(win2, image='6.png', pos=(0, 0.75), colorSpace='rgb', name='6.png')
-        trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-    if stimID == '14_7':
-        trialstim1 = visual.ImageStim(win2, image='7.png', pos=(0, 0.75), colorSpace='rgb', name='7.png')
-        trialstim2 = visual.ImageStim(win1, image='14.png', pos=(0, 0.75), colorSpace='rgb', name='14.png')
-    if stimID == '12_8':
-        trialstim1 = visual.ImageStim(win2, image='8.png', pos=(0, 0.75), colorSpace='rgb', name='8.png')
-        trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-    if stimID == '12_9':
-        trialstim1 = visual.ImageStim(win2, image='9.png', pos=(0, 0.75), colorSpace='rgb', name='9.png')
-        trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0.75), colorSpace='rgb', name='12.png')
-
-except IndexError:
-    print "you forgot to say what trial type. defaulting to 'acclim'"
-    trialstim1 = visual.ImageStim(win2, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
-    trialstim2 = visual.ImageStim(win1, image='0.png', pos=(0, 0.75), colorSpace='rgb', name='0.png')
-
-try:
-    lftstim, rgtstim = stimID.split("_")
-except NameError:
-    pass
-
-# training or probe trial/length
-try:
-    ttID = sys.argv[2]
-    if ttID == 'train':
-        tLength = 4 * 60
-    if ttID == 'probe':
-        tLength = 4 * 60
-except IndexError:
-    print "you forgot to say if this is a 'train' or 'probe' trial"
-    tLength = 1 * 60
-
-try:
-    w.writerow([date, now, stimID, ttID, pesces, trialstim1.name, trialstim1.win.screen, trialstim2.name,
-                trialstim2.win.screen])
-except NameError:
-    w.writerow([date, now, 'stimID missing', 'ttID missing', 'fishids missing', trialstim1.name, trialstim1.win.screen,
-                trialstim2.name,
-                trialstim2.win.screen])
-
-prepcam1 = subprocess.Popen(['v4l2-ctl', '-d', '/dev/video0', '--set-ctrl', 'focus_auto=0'],
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE)
-prepcam2 = subprocess.Popen(['v4l2-ctl', '-d', '/dev/video1', '--set-ctrl', 'focus_auto=0'],
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE)
-'''
-prepcam1b = subprocess.Popen(['v4l2-ctl', '-d', '/dev/video0', '--set-ctrl', 'white_balance_temperature_auto=0'],
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE)
-prepcam2b = subprocess.Popen(['v4l2-ctl', '-d', '/dev/video1', '--set-ctrl', 'white_balance_temperature_auto=0'],
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE)
-'''
-prepcam1c = subprocess.Popen(['v4l2-ctl', '-d', '/dev/video0', '--set-ctrl', 'focus_absolute=10'],
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE)
-prepcam2c = subprocess.Popen(['v4l2-ctl', '-d', '/dev/video1', '--set-ctrl', 'focus_absolute=10'],
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE)
-
-allKeys = event.waitKeys()
+            if self.stimID == '5_10':
+                self.trialstim1 = visual.ImageStim(win1, image='5.png', pos=(0, 0), colorSpace='rgb', name='5.png')
+                self.trialstim2 = visual.ImageStim(win2, image='10.png', pos=(0, 0), colorSpace='rgb', name='10.png')
+            if self.stimID == '6_12':
+                self.trialstim1 = visual.ImageStim(win1, image='6.png', pos=(0, 0), colorSpace='rgb', name='6.png')
+                self.trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0), colorSpace='rgb', name='12.png')
+            if self.stimID == '7_14':
+                self.trialstim1 = visual.ImageStim(win1, image='7.png', pos=(0, 0), colorSpace='rgb', name='7.png')
+                self.trialstim2 = visual.ImageStim(win2, image='14.png', pos=(0, 0), colorSpace='rgb', name='14.png')
+            if self.stimID == '8_12':
+                self.trialstim1 = visual.ImageStim(win1, image='8.png', pos=(0, 0), colorSpace='rgb', name='8.png')
+                self.trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0), colorSpace='rgb', name='12.png')
+            if self.stimID == '9_12':
+                self.trialstim1 = visual.ImageStim(win1, image='9.png', pos=(0, 0), colorSpace='rgb', name='9.png')
+                self.trialstim2 = visual.ImageStim(win2, image='12.png', pos=(0, 0), colorSpace='rgb', name='12.png')
+            if self.stimID == '0_0':
+                self.trialstim1 = visual.ImageStim(win1, image='0.png', pos=(0, 0), colorSpace='rgb', name='0.png')
+                self.trialstim2 = visual.ImageStim(win2, image='0.png', pos=(0, 0), colorSpace='rgb', name='0.png')
+            if self.stimID == '10_5':
+                self.trialstim1 = visual.ImageStim(win2, image='5.png', pos=(0, 0), colorSpace='rgb', name='5.png')
+                self.trialstim2 = visual.ImageStim(win1, image='10.png', pos=(0, 0), colorSpace='rgb', name='10.png')
+            if self.stimID == '12_6':
+                self.trialstim1 = visual.ImageStim(win2, image='6.png', pos=(0, 0), colorSpace='rgb', name='6.png')
+                self.trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0), colorSpace='rgb', name='12.png')
+            if self.stimID == '14_7':
+                self.trialstim1 = visual.ImageStim(win2, image='7.png', pos=(0, 0), colorSpace='rgb', name='7.png')
+                self.trialstim2 = visual.ImageStim(win1, image='14.png', pos=(0, 0), colorSpace='rgb', name='14.png')
+            if self.stimID == '12_8':
+                self.trialstim1 = visual.ImageStim(win2, image='8.png', pos=(0, 0), colorSpace='rgb', name='8.png')
+                self.trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0), colorSpace='rgb', name='12.png')
+            if self.stimID == '12_9':
+                self.trialstim1 = visual.ImageStim(win2, image='9.png', pos=(0, 0), colorSpace='rgb', name='9.png')
+                self.trialstim2 = visual.ImageStim(win1, image='12.png', pos=(0, 0), colorSpace='rgb', name='12.png')
 
 
-# hang here until trial is started
-for thisKey in allKeys:
-    if thisKey == 's':
+
+        except IndexError:
+            print "you forgot to say what trial type. defaulting to 'acclim'"
+            self.trialstim1 = visual.ImageStim(win2, image='0.png', pos=(0, 0), colorSpace='rgb', name='0.png')
+            self.trialstim2 = visual.ImageStim(win1, image='0.png', pos=(0, 0), colorSpace='rgb', name='0.png')
 
 
-        # adjust these commands based on OS and desired video format/codec.
-        '''
-        # These capture Isight cam ('0') and desktop ('1') on OSX 10.10.2
-        subprocess.Popen(['ffmpeg', '-f', 'avfoundation', '-i', '0', '-t', str(tLength / 60), '../out.mpg'],
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-        subprocess.Popen(['ffmpeg', '-f', 'avfoundation', '-i', '1', '-t', str(tLength / 60), '../out2.mpg'],
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-        '''
-        # These capture cam1 ('/dev/video0') and  cam2 ('/dev/video1') on Ubuntu with v4l2
-        p1 = subprocess.Popen(
-            ['ffmpeg', '-f', 'v4l2', '-i', '/dev/video0', '-q', '2', '-s', '1920x1080', '-t',
-             str(tLength), str(lftpez) + str(lftstim) + '.avi'],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(
-            ['ffmpeg', '-f', 'v4l2', '-i', '/dev/video1', '-q', '2', '-s', '1920x1080', '-t',
-             str(tLength), str(rgtpez) + str(rgtstim) + '.avi'],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE)
+    def writedat(self, date, now):
 
-        startT = clock.getTime()
-        while (clock.getTime() - startT) < tLength:
-            trialstim1.draw()
-            trialstim2.draw()
-            win1.flip()
-            win2.flip()
-            if not p1.poll():
-                if (clock.getTime() - startT) > tLength + 15:
-                    print 'warning, p1 requires restart'
-                    p1 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-i', '/dev/video0', '-q', '2', '-s', '1920x1080',
-                                           '-t', str(tLength-(clock.getTime() - startT)), str(lftpez) + str(lftstim) + str(clock.getTime()) + '.avi'],
-                                          stdin=subprocess.PIPE,
-                                          stdout=subprocess.PIPE)
-                    startT = clock.getTime()
-            if not p2.poll():
-                if (clock.getTime() - startT) > tLength + 15:
-                    print 'warning, p2 requires restart'
-                    p2 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-i', '/dev/video0', '-q', '2', '-s', '1920x1080',
-                                           '-t', str(tLength-(clock.getTime() - startT)), str(lftpez) + str(lftstim) + str(clock.getTime()) + '.avi'],
-                                          stdin=subprocess.PIPE,
-                                          stdout=subprocess.PIPE)
-                    startT = clock.getTime()
-            else:
+        self.w.writerow([date, now, self.day, self.session, self.lftpez, self.rgtpez, self.hstim, self.lstim,
+                    self.conditionside])
 
-                continue
+    def randori(self):
+        orifoo = [90, 180, 270, 0, 360]
+        self.ori1 = int(random.choice(orifoo))
+        self.ori2 = int(random.choice(orifoo))
+        self.trialstim1.ori = self.ori1
+        self.trialstim2.ori = self.ori2
 
 
-if prepcam1.poll():
-    prepcam1.kill()
-if prepcam2.poll():
-    prepcam2.kill()
-if prepcam1c.poll():
-    prepcam1c.kill()
-if prepcam2c.poll():
-    prepcam2c.kill()
-if p1.poll():
-    p1.kill()
-if p2.poll():
-    p2.kill()
-win1.close()
-win2.close()
-core.quit()
+    def runtry(self):
+        allKeys = event.waitKeys()
+        for thisKey in allKeys:
+            if thisKey == 's':
+                self.p1 = subprocess.Popen(
+            ['ffmpeg', '-f', 'avfoundation', '-i', '2', '-r', '30', '-q', '10', '-t', str(self.tLength),
+             str(self.lftpez)+'_'+str(self.day)+'_'+str(self.session)+'.mpg'])#,
+                # stdin=subprocess.PIPE,
+                #stdout=subprocess.PIPE)
+                self.p2 = subprocess.Popen(
+            ['ffmpeg', '-f', 'avfoundation', '-i', '0', '-r', '30', '-q', '10', '-t', str(self.tLength),
+             str(self.rgtpez)+'_'+str(self.day)+'_'+str(self.session)+'.mpg'])#,
+                                   #stdin=subprocess.PIPE,
+                                   #stdout=subprocess.PIPE)
+
+                startT = self.clock.getTime()
+                while (self.clock.getTime() - startT) < self.tLength:
+            
+            #self.win1.fullscrn=True
+            #self.win2.fullscrn=True
+            #self.win1.setPos(0.0,0.0)
+            #self.win2.setPos(0.0,0.0)
+                    restKeys = event.getKeys()
+                    self.trialstim1.draw()
+                    self.trialstim2.draw()
+                    self.win1.flip()
+                    self.win2.flip()
+                    for thatKey in restKeys:
+                        if thatKey == 'q':
+
+                            print'exiting'
+                            self.p1.communicate(input='q')
+                            self.p2.communicate(input='q')
+                            _exit()
+        self.p1.kill()
+        self.p2.kill()
+        self.w.close()
+        self.win1.close()
+        self.win2.close()
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-lf","--leftFish", help="ID of fish in left tank")
+    ap.add_argument("-rf", "--rightFish", help="ID of fish in right tank")
+    ap.add_argument("-hs", "--highStim", help="high numerosity stimulus, e.g. 12")
+    ap.add_argument("-ls", "--lowStim", help="low numerosity stimulus, e.g. 6")
+    ap.add_argument("-d","--day", help="experiment day, e.g. 1-7")
+    ap.add_argument("-s","--session", help="trial session, e.g. 1-4")
+    ap.add_argument("-fs","--fedSide", help="side feed on/conditioned side")
+    args = vars(ap.parse_args())
+    RT = Trial(time.strftime('%m%d%Y'), time.strftime('%X'), args["day"], args["session"], args["leftFish"],
+               args["rightFish"], args["highStim"], args["lowStim"], args["fedSide"])
+    RT.randori()
+    RT.writedat(time.strftime('%m%d%Y'), time.strftime('%X'))
+    while 1:
+
+        subprocess.Popen(["osascript", "screenset.scpt"])
+
+        RT.runtry()
+
+        core.quit()
+        _exit()
