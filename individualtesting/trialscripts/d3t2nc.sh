6307e64e742ddff716c2df5780fd654c0fc6129c
robertIan <rianetheredge@gmail.com>
2016-05-18 12:10:11 -0500

    fixed problems in master.txt in individualtesting/trialscripts then ran createtrialscript.sh again

--- individualtesting/trialscripts/d3t2nc.sh
+++ individualtesting/trialscripts/d3t2nc.sh
@@ -1,3 +1,3 @@
 export DISPLAY=:0.0
 cd /home/pi/ethoStim/individualtesting
-d=$(date --date='TZ="US/Central" May 20 11:05:00 2016' +%s); python trial.py -f Second -ps 6.png -ts 12 -d 3 -s 2 -fs right-x female -p 50 -sp gambusia -sl 367 -r 1  -m $d
+d=$(date --date='TZ="US/Central" May 20 11:05:00 2016' +%s); python trial.py -f Second -ps 6.png -ts 12 -d 3 -s 2 -fs right -x female -p 50 -sp gambusia -sl 367 -r 1 -m $d
