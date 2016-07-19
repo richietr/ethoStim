5d74ac9e0cd046ea2322a15a26d3dbcb7c2046a2
robertIan <rianetheredge@gmail.com>
2016-07-19 13:57:41 -0500

    fixed typos in L1 (Lautoka)

--- individualtesting/trialscripts/d8t5c.sh
+++ individualtesting/trialscripts/d8t5c.sh
@@ -1,3 +1,3 @@
 export DISPLAY=:0.0
 cd /home/pi/ethoStim/individualtesting
-d=$(date --date='TZ="US/Central" Sat July 23 17:05:00 2016' +%s); python trial.py -f Harlow -ps 6.png -ts 12 -d 8 -s 5 -fs left -x female -p 50 -sp gambusia -sl 328 -r 3 -cs L -c -m $d
+d=$(date --date='TZ="US/Central" Sat July 23 17:05:00 2016' +%s); python trial.py -f Harlow -ps 12.png -ts 6 -d 8 -s 5 -fs left -x female -p 50 -sp gambusia -sl 328 -r 3 -cs L -c -m $d
--- individualtesting/trialscripts/d9t4c.sh
+++ individualtesting/trialscripts/d9t4c.sh
@@ -1,3 +1,3 @@
 export DISPLAY=:0.0
 cd /home/pi/ethoStim/individualtesting
-d=$(date --date='TZ="US/Central" Sun July 24 15:05:00 2016' +%s); python trial.py -f Harlow -ps 6.png -ts 12 -d 9 -s 4 -fs left -x female -p 50 -sp gambusia -sl 328 -r 3 -cs R -c -m $d
+d=$(date --date='TZ="US/Central" Sun July 24 15:05:00 2016' +%s); python trial.py -f Harlow -ps 6.png -ts 12 -d 9 -s 4 -fs left -x female -p 50 -sp gambusia -sl 328 -r 3 -cs R -fd -c -m $d
